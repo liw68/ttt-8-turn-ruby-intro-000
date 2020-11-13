957871f29234493a8ca19aa405a54bbca32b8864
Liam Walsh <liw6800@gmail.com>
2020-11-13 23:43:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,6 +46,7 @@ def turn (board)
   index = input_to_index (input)
   until valid_move?(board, index) == true
     turn (board)
+  end  
   move (board, index, current_players = "X")
   display_board (board)
   end
