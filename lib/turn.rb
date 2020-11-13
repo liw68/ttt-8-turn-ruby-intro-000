383a45e1462d811f3d79eb8ca6c784148ae0c241
Liam Walsh <liw6800@gmail.com>
2020-11-13 23:34:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,7 @@ def turn (board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index (input)
-  until valid_move? == true
+  until valid_move?(board, index) == true
     puts "Please enter 1-9:"
   end
 end
